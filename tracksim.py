7261a6d79122c59c700bb684ae1539d75dad28f5
ArjunAlbertDev <34843324+ArjunAlbertDev@users.noreply.github.com>
2020-06-05 16:30:19 -0400

    Removed deadcode

--- tracksim.py
+++
@@ -1,8 +0,0 @@
-# units are meters and radians
-long_straightaway_length = 1006
-long_straightaway_width = 15
-short_straightaway_length = 201
-short_straightaway_width = 15
-turn_length = 402
-turn_width = 18
-turn_banking = 0.1606
